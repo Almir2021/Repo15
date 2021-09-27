907f21446cde3f96b45f69a8ccee1978e3b6e014
Almir2021 <bosnjicalmir@gmail.com>
2021-09-27 22:14:47 +0200

    Vjezba 4

---
+++ ex04/src/index.js
@@ -0,0 +1,21 @@
+const milili = [10,25,4];
+
+function myArr (mili) {
+    `use strict`;
+
+    // Only change code below this line
+     for (let i = 0; i< mili.length; i++) {
+         milili[i] = mili[i]; 
+         
+         
+     }
+     return milili;
+     
+
+// Only change code above this line
+
+    
+
+} 
+ 
+console.log (myArr([4,10,25]))
